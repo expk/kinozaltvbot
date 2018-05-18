f5ad6bbaa7568a48b46774aa9bf34f927af76078
expk <forensic063@gmail.com>
2018-05-18 15:14:44 +0400

    first commit

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def get_all_links(html, maxsearch):
     # except:
     #     print('По вашему запросу ничего не обнаружено')
     #     exit(0)
-    #soup = BeautifulSoup(html, 'lxml')
-    print(soup)
+    soup = BeautifulSoup(html, 'lxml')
+    #print(soup)
     tds = soup.find('table', class_='t_peer w100p').find_all('td', class_='nam')
     links = []
 
